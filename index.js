ba78547a7d64becc3905f1f304d94462e2460b8b
Jak-Turnbull <jak.turnbull1@gmail.com>
2022-08-23 23:22:34 +0100

    added nodelist iterator to add event listeners to each button

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-
-document.querySelector("button").addEventListener("click", handleClick);
-
-function handleClick(){
-  alert ("I got clicked");
+//The code below loops through the number of buttons present on the webpage
+//for each button it adds an event listener that makes an alert pop up when clicked
+//the buttons are stored querySelectorAll node list the loop iterates through this list
+for (let i = 0; i <= document.querySelectorAll(".drum").length; i++) {
+ document.querySelectorAll("button")[i].addEventListener("click", function () {
+   alert("I got clicked");
+ });
 }
