cee36e6f8ff7efb5a4d19fa10fb50592e1b2bf8a
Jak-Turnbull <jak.turnbull1@gmail.com>
2022-08-25 21:40:43 +0100

    button now changes text color have commented out the code that plays the sound

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 //The code below loops through the number of buttons present on the webpage
 //for each button it adds an event listener that makes an alert pop up when clicked
-//the buttons are stored querySelectorAll node list the loop iterates through this list
+//the buttons are stored querySelectorAll node list the loop iterates through
+
+
 
 for (let i = 0; i <= document.querySelectorAll(".drum").length; i++) {
  document.querySelectorAll("button")[i].addEventListener("click", function () {
-   var audio = new audio('sounds/crash.mp3');
-   audio.play();
-   console.log("iamsam");
+this.style.color = "white";
  });
 }
+
+
+
+
+
+
+
+//var song = new Audio("sounds/tom-1.mp3");
+//song.play();
