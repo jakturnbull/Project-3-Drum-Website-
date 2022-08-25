3fc349910fd2d43188d0c30529bfe1f22fd58c4e
Jak-Turnbull <jak.turnbull1@gmail.com>
2022-08-25 13:21:08 +0100

    mid attempt for getting audio to paly on button click

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 //The code below loops through the number of buttons present on the webpage
 //for each button it adds an event listener that makes an alert pop up when clicked
 //the buttons are stored querySelectorAll node list the loop iterates through this list
+
 for (let i = 0; i <= document.querySelectorAll(".drum").length; i++) {
  document.querySelectorAll("button")[i].addEventListener("click", function () {
-   alert("I got clicked");
+   var audio = new audio('sounds/crash.mp3');
+   audio.play();
+   console.log("iamsam");
  });
 }
