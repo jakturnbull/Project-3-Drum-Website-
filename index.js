730506d74ca64260e074f9e21310d63f599e0056
Jak-Turnbull <jak.turnbull1@gmail.com>
2022-08-29 13:52:41 +0100

    added keydown for console loging need to add that function to rest of website

--- index.js
+++ index.js
@@ -2,61 +2,63 @@
 //for each button it adds an event listener that makes an alert pop up when clicked
 //the buttons are stored querySelectorAll node list the loop iterates through
 
-
+document.addEventListener("keydown", function(event) {
+  console.log("key was pressed");
+  console.log(event);
+});
 
 for (let i = 0; i <= document.querySelectorAll(".drum").length; i++) {
- document.querySelectorAll("button")[i].addEventListener("click", function () {
+  document.querySelectorAll("button")[i].addEventListener("keydown", function(event) {
 
-   var buttonInnerHTML = this.innerHTML;
+    var buttonInnerHTML = this.innerHTML;
 
-      switch (buttonInnerHTML) {
+    switch (buttonInnerHTML) {
 
-        case "w":
-            var snare = new Audio("sounds/snare.mp3");
-            snare.play();
-          break;
+      case "w":
+        var snare = new Audio("sounds/snare.mp3");
+        snare.play();
+        break;
 
-          case "a":
-              var kick_bass = new Audio("sounds/kick_bass.mp3");
-              kick_bass.play();
-            break;
+      case "a":
+        var kick_bass = new Audio("sounds/kick_bass.mp3");
+        kick_bass.play();
+        break;
 
-            case "s":
-                var tom1 = new Audio("sounds/tom_1.mp3");
-                tom1.play();
-              break;
+      case "s":
+        var tom1 = new Audio("sounds/tom_1.mp3");
+        tom1.play();
+        break;
 
-              case "d":
-                  var tom2 = new Audio("sounds/tom_2.mp3");
-                  tom2.play();
-                break;
+      case "d":
+        var tom2 = new Audio("sounds/tom_2.mp3");
+        tom2.play();
+        break;
 
-                case "j":
-                    var tom3 = new Audio("sounds/tom_3.mp3");
-                    tom3.play();
-                  break;
+      case "j":
+        var tom3 = new Audio("sounds/tom_3.mp3");
+        tom3.play();
+        break;
 
-                  case "k":
-                      var song = new Audio("sounds/tom_4.mp3");
-                      song.play();
-                    break;
+      case "k":
+        var song = new Audio("sounds/tom_4.mp3");
+        song.play();
+        break;
 
-                    case "l":
-                        var song = new Audio("sounds/crash.mp3");
-                        song.play();
-                      break;
+      case "l":
+        var song = new Audio("sounds/crash.mp3");
+        song.play();
+        break;
 
-        default: console.log(buttonInnerHTML);
+      default:
+        console.log(buttonInnerHTML);
 
-      }
+    }
 
- });
+  });
 }
 
 
-document.addEventListener("keypress", function() {
-  alert("key was pressed");
-});
+
 
 
 
